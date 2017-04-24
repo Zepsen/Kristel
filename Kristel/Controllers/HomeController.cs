31077f53503f535b31ea54d723477bad148240f9
Zepsen Dmytro <zepsendm1@gmail.com>
2017-04-24 17:39:07 +0300

    Create MVC

---
+++ Kristel/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kristel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()  => View();        
+    }
+}
